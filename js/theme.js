a36bb40bf6834ba1e9c59cd93273201226fdd45f
lara <laracome1998@gmail.com>
2024-05-24 20:34:36 +0200

    Corrección errores web sin modificación documento explicativo

---
+++ js/theme.js
@@ -0,0 +1,10 @@
+const toggleButton = document.getElementById('toggleMode');
+
+toggleButton.addEventListener('click', () => {
+    if (document.body.getAttribute('data-theme') === 'dark') {
+        document.body.removeAttribute('data-theme');
+    } else {
+        document.body.setAttribute('data-theme', 'dark');
+    }
+});
+
